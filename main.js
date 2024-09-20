b0b84ab1aade6b12fc639b7f0a166f6d0872857e
mamunurrashid8206 <mamunurrashid8206@gmail.com>
2024-09-21 02:15:11 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// // let saraHave= 25;
+// // let saraBought= 15;
+
+// // let totalApple = saraHave + saraBought;
+// // let finalResult = ('saraHave total: ' + totalApple + ' apples')
+// // console.log(finalResult)
+
+
+
+// // let jonHave = 500;
+// // let jonExpenditure = 200;
+
+// // let totalHave = jonHave - jonExpenditure;
+// // let finalResult = ('jonHave total amount: ' + totalHave + ' taka')
+// // console.log(finalResult);
+
+
+// // let makesMachinePerHour = 120;
+// // let hour = 8;
+// // let totalMakes = makesMachinePerHour * hour
+// // let finalResult = ('Makes machine ' +  totalMakes  + ' units in ' +  hour  + ' hours.')
+// // console.log(finalResult);
+
+
+// // let treesPerRow = 12;
+// // let rows = 8;
+// // let additionalTrees = 20;
+// // let totalTrees = treesPerRow * rows;
+// // totalTrees += additionalTrees;
+// // console.log("The total number of trees in the garden will be " + totalTrees + '.');
+
+
+
+// // let rohimsHaveBox = 5;
+// // let orangeHavePerBox = 10;
+// // let friendsGave = 12;
+// // let totalHave = rohimsHaveBox * orangeHavePerBox;
+// // totalHave -= friendsGave;
+// // console.log("Rohims have total orange " + totalHave + ".");
+
+
+let distance = 150;
+let time = 3;
+let speed = distance/time;
+console.log('The train covers ' + speed + ' km per hour. ');
+
+
+
+// let machines = 6;
+// let productsPerDay = 200;
+// let additionalMachines = 2;
+// let unsoldMachines = 3;
+// let productionPerMachine = productsPerDay / machines;
+// let totalMachine = machines + additionalMachines;
+// let totalProduction = productionPerMachine * totalMachine;
+// let remainingProducts = totalProduction - unsoldMachines;
+// console.log ('The total production with 2 more machines is ' + totalProduction + ' products.')
+// console.log("The remaining products after 3 unsold items is " + remainingProducts + " products.");
